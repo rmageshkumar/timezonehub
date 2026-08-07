af53296bd87f6632da659aa4ecb2a69c88eb7197
Mageshkumar R <mageshkumar.it@gmail.com>
2026-08-07 12:51:30 +0530

    Added banners and config

--- next.config.js
+++ next.config.js
@@ -44,6 +44,12 @@ const nextConfig = {
         ],
       },
       {
+        source: "/_next/static/:path*",
+        headers: [
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+        ],
+      },
+      {
         source: "/(.*)",
         headers: [
           { key: "X-Frame-Options", value: "DENY" },
